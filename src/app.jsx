87492fd78c226654e3d0d9657b5ed9d7c3ce5632
Ann <annstramer@pm.me>
2025-09-19 15:26:28 -0500

    implement and animate art section

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 // register here to make these globally, across application
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -16,6 +17,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
         </main>
     )
 }
